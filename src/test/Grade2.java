7d5f3acba6cf011c6bfbeeea7261d82b7e34a1dc
fhfh5 <fhfh5@DESKTOP-IFRS6NC>
2024-02-18 23:24:13 +0900

    upload

--- src/test/Grade2.java
+++ src/test/Grade2.java
@@ -1,9 +1,80 @@
 package test;
 
+import java.util.ArrayList;
+
 public class Grade2 {
+	String name;
+	double math = 0;
+	double English = 0;
+	double language = 0;
+	
+	public Grade2(String name,double math,double English,double language) {
+		this.name = name;
+		this.math = math;
+		this.English =English;
+		this.language = language;	
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getMath() {
+		return math;
+	}
+	public void setMath(double math) {
+		this.math = math;
+	}
+	
+	public double getEnglish() {
+		return English;
+	}
+	public void setEnglish(double english) {
+		English = english;
+	}
+	
+	public void setLanguag(double language) {
+		this.language = language;
+	}
+	public double getLanguage() {
+		return language;
+	}
+	
+	public void setLanguage(double language) {
+		this.language = language;
+	}
+	
+	public void score() {
+		ArrayList<Double> arr = new ArrayList<>();
+		arr.add(math);
+		arr.add(English);
+		arr.add(English);
+		
+		for(int i = 0 ; i < arr.size() ; i++)
+		{
+			System.out.print(String.format("%-10s",arr.get(i)));
+		}
+	}
+	public void scoresum() {
+		double sum = 0;
+		sum = math + English + language;
+		System.out.print(String.format("%-10s", sum));
+		sum = sum/3;
+		System.out.print(String.format("%-10s", sum));
+		
+	}
+	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Grade2 g1 = new Grade2("Tong", 85, 35, 78);
+		
+		g1.score();
+		g1.scoresum();
 
 	}
 
--- src/test/Pyramid.java
+++ src/test/Pyramid.java
@@ -6,18 +6,18 @@ public class Pyramid {
 		// TODO Auto-generated method stub
 		int num = 5;
 		
-		for(int i = 0 ;i < num ; i++)
-		{		
-			for(int j = 0 ; j < num - i ; j++)
-			{
-				System.out.print(" ");
-			}
-			for(int c = 0 ; c<i+(i+1); c++)
-			{
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+//		for(int i = 0 ;i < num ; i++)
+//		{		
+//			for(int j = 0 ; j < num - i ; j++)
+//			{
+//				System.out.print(" ");
+//			}
+//			for(int c = 0 ; c<i+(i+1); c++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 		
 		
 		
@@ -33,14 +33,14 @@ public class Pyramid {
 		
 		
 		
-//		for(int i = 0 ; i<=num ; i++)
-//		{
-//			for(int j = 0 ;  j <= i; j++)
-//			{
-//				System.out.print("*");
-//			}
-//			System.out.println();
-//		}
+		for(int i = 0 ; i<=num && i<= 10 ; i++)
+		{
+			for(int j = 0 ;  j <= i;  j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
 
 		
